c833522428fdc7649c336ecd1aa26e5e8a861a8d
root <root@builder.thoughtworks.com>
2012-07-10 07:46:10 -0700

    Modified stop script to exit gracefully

--- stop-db.sh
+++ stop-db.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+if [ -f "nohup.out" ]; then
 java -cp h2*.jar org.h2.tools.Server -tcpShutdown tcp://localhost:9092 -tcpPassword badger
+rm nohup.out
+else
+  echo "Sorry! No running H2 instances found"
+fi
