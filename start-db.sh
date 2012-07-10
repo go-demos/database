4ff3daa86075720c3d6d6606022a23b8d98620ff
Jairaj Menon <jairaj.menon@gmail.com>
2012-07-10 08:10:25 -0700

    Modified startup script to exit gracefully

--- start-db.sh
+++ start-db.sh
@@ -1,1 +1,2 @@
-nohup java -cp h2*.jar org.h2.tools.Server -tcp -tcpAllowOthers -tcpPassword badger &
+#!/bin/bash
+nohup java -cp h2*.jar org.h2.tools.Server -tcp -tcpAllowOthers -tcpPassword badger >>nohup.out 2>&1 &
