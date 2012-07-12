7011836eeaf6d7c9a88ddf7591ee694fe9887cd0
Pavan Sudarshan <itspanzi@gmail.com>
2012-07-12 05:47:38 +0530

    Made stop resilient to no service running

--- stop-db.sh
+++ stop-db.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-if [ -f "nohup.out" ]; then
-java -cp h2*.jar org.h2.tools.Server -tcpShutdown tcp://localhost:9092 -tcpPassword badger
-rm nohup.out
+
+process=org.h2.tools.Server
+pid=`ps axwww | grep java | grep ${process} | awk '{ print $1 };'`
+
+if [ "${pid}x" = "x" ]
+then
+ echo "No H2 Server running."
 else
-  echo "Sorry! No running H2 instances found"
+    echo "Shutting down H2 Server process id: ${pid}"
+    java -cp h2*.jar org.h2.tools.Server -tcpShutdown tcp://localhost:9092 -tcpPassword badger
+    echo "Finished shutting"
 fi
