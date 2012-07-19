2a80f728337ab8ebaa541a5d1202b6d6d0d28f31
Pavan Sudarshan <itspanzi@gmail.com>
2012-07-19 06:16:20 +0530

    Copy over h2 jars before starting server

--- start-db.sh
+++ start-db.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /usr/local/database
+[ ! -z $DB_DIR ] || DB_DIR=/usr/local/database
+cp h2*.jar $DB_DIR
+cd $DB_DIR
 nohup java -cp h2*.jar org.h2.tools.Server -tcp -tcpAllowOthers -tcpPassword badger >>nohup.out 2>&1 &
